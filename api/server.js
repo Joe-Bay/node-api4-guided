579ed977f41fb8304f4dbf7182a15245cfe0c717
Joe Bay <jbay55278@gmail.com>
2020-09-03 09:50:02 -0800

    add greet endpoint

--- api/server.js
+++ api/server.js
@@ -7,6 +7,11 @@ const server = express();
 
 server.use(helmet());
 
+server.use('/greet/:name', (req, res) => {
+    const greeting = process.env.GREETING || 'Hello'
+    res.status(200).send(`<h1>${greeting} ${req.params.name}</h1>`)
+})
+
 server.use('/api', apiRouter);
 
 module.exports = server;
